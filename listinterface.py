e333927769fafdea00ed763a2f4635a9a4bd2e89
doadin <tbkizle@gmail.com>
2022-06-05 21:54:14 -0400

    Update listinterface.py

--- listinterface.py
+++ listinterface.py
@@ -3,7 +3,6 @@ def listinterface():
     import ifaddr
 
     adapters = ifaddr.get_adapters()
-    interface = interface.encode('utf-8')
     for adapter in adapters:
         print("------------")
         print(adapter.name)
